6dfbaa7616c61c08565003df5e1694ca9dbd4299
lepc53 <sasquach53@hotmail.com>
2021-02-04 14:32:51 -0500

    0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the function main prints all alphabet
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; ++n)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
