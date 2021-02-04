ab74ae20bbafbc618e4945faf8a81e99be64c4e9
lepc53 <sasquach53@hotmail.com>
2021-02-04 16:35:55 -0500

    numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the function main use printf to print numbers
+ * more nuuuuuumbers
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
