0fb3065502e331321e0a3e13eb8aacfab7199736
lepc53 <sasquach53@hotmail.com>
2021-02-04 14:19:50 -0500

    1-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the function main use printf to print numbers
+ * del 1 al 10
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
