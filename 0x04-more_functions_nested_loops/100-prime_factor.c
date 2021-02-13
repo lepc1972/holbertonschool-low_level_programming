6f82dd15c32e10e2f4f210f9f7d849429006de3b
lepc53 <sasquach53@hotmail.com>
2021-02-12 21:12:25 -0500

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main -  the largest factor o  612852475143
+* Return: 0
+**/
+
+int main(void)
+{
+	long int num1, test_num, least_factor;
+
+	num1 = 2;
+	least_factor = 2;
+	test_num = 612852475143;
+
+	while (test_num > num1)
+	{
+		if (test_num % num1 == 0)
+		{
+			least_factor = 2;
+			test_num = test_num / num1;
+			num1 = num1 * num1;
+		}
+		else
+		{
+			num1++;
+		}
+	}
+	least_factor = num1;
+	printf("%ld\n", least_factor);
+	return (0);
+}
