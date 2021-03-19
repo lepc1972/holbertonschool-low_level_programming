4ee6126b8781aa4c33202eca3cf31e6b95d16f20
lepc53 <sasquach53@hotmail.com>
2021-03-19 11:03:26 -0500

    linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,32 +10,32 @@
 
 size_t print_list(const list_t *h)
 {
-    const list_t *act = h;
-    int num = 0;
+const list_t *act = h;
+int num = 0;
 
-    if (act == NULL)
-    {
-        return (0);
-    }
-    
-    
-    while (act != NULL) 
-    {
-            num++;
-            if (act->str == NULL)
-            {
-                printf("[0] ");
-		printf("(nil)\n");            
-            }
-            else
-            {
-                printf("[%d] ", act->len);
-                printf("[%s\n]", act->str);
-            }
-            act = act->next;
+if (act == NULL)
+{
+return (0);
+}
 
-    }
-    return (num);
-    
+
+while (act != NULL)
+{
+num++;
+if (act->str == NULL)
+{
+printf("[0] ");
+printf("(nil)\n");
+}
+else
+{
+printf("[%d] ", act->len);
+printf("[%s\n]", act->str);
+}
+act = act->next;
+
+}
+return (num);
+
 
 }
