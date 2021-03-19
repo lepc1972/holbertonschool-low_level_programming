10a7fb7021ee7ca499c3a1cbb21c958b31c7b5f9
lepc53 <sasquach53@hotmail.com>
2021-03-19 09:40:19 -0500

    lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+
+/**
+ * print_list - Function that prints all the elements of a list_t list
+ * @h: Pointed to head of a list
+ *
+ * Return: The number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+    const list_t *act = h;
+    int num = 0;
+
+    if (act == NULL)
+    {
+        return (0);
+    }
+    
+    
+    while (act != NULL) 
+    {
+            num++;
+            if (act->str == NULL)
+            {
+                printf("[0] ");
+		printf("(nil)\n");            
+            }
+            else
+            {
+                printf("[%d] ", act->len);
+                printf("[%s\n]", act->str);
+            }
+            act = act->next;
+
+    }
+    return (num);
+    
+
+}
