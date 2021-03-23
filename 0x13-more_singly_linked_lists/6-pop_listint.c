93cc8b19375063161bcdaed642d9c0b2f95962b7
lepc53 <sasquach53@hotmail.com>
2021-03-23 12:31:31 -0500

    fixed

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,20 +8,20 @@
  */
 int pop_listint(listint_t **head)
 {
-    int n = 0;
-    listint_t *nuevo;
+int n = 0;
+listint_t *nuevo;
 
-    if (*head == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        n = (*head)->n;
-        nuevo = (*head)->next;
-        free(*head);
-        *head = nuevo;
+if (*head == NULL)
+{
+return (0);
+}
+else
+{
+n = (*head)->n;
+nuevo = (*head)->next;
+free(*head);
+*head = nuevo;
 
-        return (n);
-    }
+return (n);
+}
 }
