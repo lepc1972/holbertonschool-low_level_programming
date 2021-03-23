824d94e57d1865a229e83949817d837eb45d4d97
lepc53 <sasquach53@hotmail.com>
2021-03-23 09:37:01 -0500

    fix-lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
-#ifndef  _LISTS_H_
-#define  _LISTS_H_
+#ifndef LISTS_H_
+#define LISTS_H_
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
@@ -15,9 +15,11 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+
+/* Prototype */
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -25,8 +27,10 @@ listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
-
-
-
-#endif // !_LISTS.H_
+#endif
