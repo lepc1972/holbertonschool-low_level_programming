759a0544bed926791bd03ac5b4b0156729725936
lepc53 <sasquach53@hotmail.com>
2021-03-26 18:31:49 -0500

    endiann

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -11,5 +11,6 @@ int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_endianness(void);
 
 # endif
