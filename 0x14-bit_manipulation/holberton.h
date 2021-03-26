4ebe8ce2309dd0b2e7116612e557bb41c7fb9f89
lepc53 <sasquach53@hotmail.com>
2021-03-25 21:32:18 -0500

    index

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -11,6 +11,7 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 
